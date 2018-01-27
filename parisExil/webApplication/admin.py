34edf34fe9796fafdaf82dcec12fc76d8f9d4dd8
maximeG <maxime.guenego@gmail.com>
2018-01-27 18:36:52 +0100

    Début de css...

--- parisExil/webApplication/admin.py
+++ parisExil/webApplication/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Nationalite,Hebergeur,Membre,Jeune,Langue,Avocat,Ecole,Disponibilite
+from .models import Nationalite, Hebergeur, Membre, Jeune, Langue, Avocat, Ecole, Disponibilite
 
 admin.site.register(Nationalite)
 admin.site.register(Hebergeur)
