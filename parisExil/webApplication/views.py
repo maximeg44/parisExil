e71ad36ac5ffb7c66939ae553eabfe796c90c144
maximeG <maxime.guenego@gmail.com>
2018-01-27 17:39:29 +0100

    Modification page accueil pour afficher nom + prenom d'un jeune

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -2,9 +2,10 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Jeune
 from lib2to3.fixes.fix_input import context
+from webApplication.models import Personne
 
 def index(request):
-    jeunes_fin_hebergement_list = Jeune.objects.all()
+    jeunes_fin_hebergement_list = Personne.objects.all()
     template = loader.get_template('webApplication/index.html')
     context = {
         'jeunes_fin_hebergement_list' : jeunes_fin_hebergement_list,
