54963528e6a67779d6be0a113af79958dc330a5d
maximeG <maxime.guenego@gmail.com>
2018-01-27 16:20:23 +0100

    Ajout d'un utilisateur administrateur du site + définition des models dans le portail d'admin

--- parisExil/webApplication/admin.py
+++ parisExil/webApplication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Nationalite,Hebergeur,Membre,Jeune,Langue,Avocat,Ecole,Disponibilite
 
-# Register your models here.
+admin.site.register(Nationalite)
+admin.site.register(Hebergeur)
+admin.site.register(Membre)
+admin.site.register(Jeune)
+admin.site.register(Langue)
+admin.site.register(Avocat)
+admin.site.register(Ecole)
+admin.site.register(Disponibilite)
+
