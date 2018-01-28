1377c62cb8e8aef3a5a065535dd08343c99dadf0
maximeG <maxime.guenego@gmail.com>
2018-01-28 18:10:22 +0100

    Tentative parsing requête sql...

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -4,13 +4,17 @@ from .models import Personne, Jeune
 from django.shortcuts import render, get_object_or_404
 from webApplication.models import Accueillir
 from django.db import connection
+from collections import namedtuple
 
 def index(request):
     #jeunes_fin_hebergement_list = Jeune.objects.all()
+    #jeunes_fin_hebergement_list = {'nom': 'nom', 'prenom' :'prenom', 'dateFin' : 'dateFin', 'idPersonne': 'idPersonne'}
     with connection.cursor() as cursor:
         cursor.execute("SELECT nom, prenom, accueillir.dateFin, Personne.idPersonne FROM accueillir, Personne where Personne.idPersonne = accueillir.idPersonne")
         row = cursor.fetchall()
-    jeunes_fin_hebergement_list = row[0]
+        cursor.close()
+    
+    jeunes_fin_hebergement_list = row
     template = loader.get_template('webApplication/index.html')
     context = {
         'jeunes_fin_hebergement_list' : jeunes_fin_hebergement_list,
