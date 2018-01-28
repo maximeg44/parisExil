75fcaaa461e540f62c69f013eba78f2b796960ec
maximeG <maxime.guenego@gmail.com>
2018-01-28 17:28:24 +0100

    Récupération d'information supplémentaire via requête sql

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -2,11 +2,15 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Personne, Jeune
 from django.shortcuts import render, get_object_or_404
-import datetime
 from webApplication.models import Accueillir
+from django.db import connection
 
 def index(request):
-    jeunes_fin_hebergement_list = Jeune.objects.all()
+    #jeunes_fin_hebergement_list = Jeune.objects.all()
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT nom, prenom, accueillir.dateFin, Personne.idPersonne FROM accueillir, Personne where Personne.idPersonne = accueillir.idPersonne")
+        row = cursor.fetchall()
+    jeunes_fin_hebergement_list = row[0]
     template = loader.get_template('webApplication/index.html')
     context = {
         'jeunes_fin_hebergement_list' : jeunes_fin_hebergement_list,
