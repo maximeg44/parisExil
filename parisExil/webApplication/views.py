252c69fe67532316970c8428eae4f00db1f44c2d
maximeG <maxime.guenego@gmail.com>
2018-01-29 19:16:37 +0100

    mise au propre du code dans le controleur

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -11,23 +11,10 @@ from test.datetimetester import DAY
 from _datetime import timedelta
 
 def index(request):
-    #jeunes_fin_hebergement_list = Jeune.objects.all()
-    #jeunes_fin_hebergement_list = {'nom': 'nom', 'prenom' :'prenom', 'dateFin' : 'dateFin', 'idPersonne': 'idPersonne'}
-    #jeunes_fin_hebergement_list = namedtuple('J', 'nom, prenom, dateFin, id')
-    #with connection.cursor() as cursor:
-        #cursor.execute("SELECT nom, prenom, accueillir.dateFin, Personne.idPersonne FROM accueillir, Personne where Personne.idPersonne = accueillir.idPersonne")
-        #jeunes_fin_hebergement_list = cursor.fetchall()
-        #cursor.close()
-    
-    #jeunes_fin_hebergement_list = Accueillir.objects.all()
     start_date = datetime.today()
     delai = timedelta(7)
     end_date = start_date + delai
-    #jeunes_fin_hebergement_list = Accueillir.objects.filter(datefin = [start_date,end_date])
     jeunes_fin_hebergement_list = Accueillir.objects.all().filter(datefin__lte=end_date) 
-    
-    
-   # jeunes_fin_hebergement_list = row
     template = loader.get_template('webApplication/index.html')
     context = {
         'jeunes_fin_hebergement_list' : jeunes_fin_hebergement_list,
