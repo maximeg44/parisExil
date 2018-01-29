7f329931837453e4987a1c726e333a2e275f97ab
maximeG <maxime.guenego@gmail.com>
2018-01-29 11:37:49 +0100

    Mise à jour du controleur pour l'affichage d'un jeune en fin d'hébergement

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -9,11 +9,13 @@ from collections import namedtuple
 def index(request):
     #jeunes_fin_hebergement_list = Jeune.objects.all()
     #jeunes_fin_hebergement_list = {'nom': 'nom', 'prenom' :'prenom', 'dateFin' : 'dateFin', 'idPersonne': 'idPersonne'}
-    jeunes_fin_hebergement_list = namedtuple('J', 'nom, prenom, dateFin, id')
-    with connection.cursor() as cursor:
-        cursor.execute("SELECT nom, prenom, accueillir.dateFin, Personne.idPersonne FROM accueillir, Personne where Personne.idPersonne = accueillir.idPersonne")
-        jeunes_fin_hebergement_list = cursor.fetchall()
-        cursor.close()
+    #jeunes_fin_hebergement_list = namedtuple('J', 'nom, prenom, dateFin, id')
+    #with connection.cursor() as cursor:
+        #cursor.execute("SELECT nom, prenom, accueillir.dateFin, Personne.idPersonne FROM accueillir, Personne where Personne.idPersonne = accueillir.idPersonne")
+        #jeunes_fin_hebergement_list = cursor.fetchall()
+        #cursor.close()
+    
+    jeunes_fin_hebergement_list = Accueillir.objects.all()
 
         
     
