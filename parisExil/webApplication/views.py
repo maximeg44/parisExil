bdfb8c893f1e3db6995e2db3ec1f06c6f8c8004f
vlcpl <valentin.lecouple@gmail.com>
2018-02-02 10:43:00 +0100

    Modification de l'algorithme de vérification de l'état d'hebergement d'un jeune
    Et indentation de detailJeune.html

--- parisExil/webApplication/views.py
+++ parisExil/webApplication/views.py
@@ -59,8 +59,11 @@ def listeHebergeurs(request, hebergeur_id=None):
 #Cette méthode permet de récupérer la totalité des jeunes dans la base de données
 #Méthode à paramètre facultatif: si un id est donné en paramètre, on cherche le jeune correspondant à cet id pour en afficher le détail du profil
 def listeJeunes(request, jeune_id = None):
+    start_date = datetime.today()
+    delai = timedelta(7)
+    end_date = start_date + delai
     jeunes_list = Jeune.objects.all()
-    jeunes_heberges_list = Accueillir.objects.values_list('idpersonne',flat=True)
+    jeunes_heberges_list = Accueillir.objects.values_list('idpersonne',flat=True).filter(datefin__lte=end_date)
     template = loader.get_template('webApplication/listeJeunes.html')
     context={}
     context['jeunes_list'] = jeunes_list
